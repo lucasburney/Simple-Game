249b896ef8643f49082961ec42fcc3af96648384
Lucas Burney <burnelucasd@gmail.com>
2022-10-06 18:30:23 -0500

    Initialize main files

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("gameArea");
+const ctx = canvas.getContext('2d'); // ctx means context. The context of our drawings are two dimensional
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0,0, canvas.width, canvas.height);
+
+
+
